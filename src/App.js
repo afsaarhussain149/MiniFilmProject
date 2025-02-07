e3b27a86416e1e5c24c061086df1724470fef1ad
afsaarhussain149 <afsaarhussain149@gmail.com>
2025-02-07 12:38:28 +0530

    AS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Home from './modules/Home';
-import MoviesZone from './MoviesZone/MoviesZone';
-import FirstApi from './modules/Api/FirstApi';
-import Form from './modules/Home/Form';
-import ImanageEmployeeApi from './modules/Api/ImanageEmployeeApi';
-import MultiColumn from './modules/Home/MultiColumn';
-import Food from './Food/Food';
+
 
 
 function App() {
